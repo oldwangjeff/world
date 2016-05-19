c62bcde8bc65592bba454c0aaa156a75686438f2
oldwangjeff <864636467@qq.com>
2016-05-20 00:49:43 +0800

    a.c commit

---
+++ a.c
@@ -0,0 +1,17 @@
+ /************************************************************************
+ > File Name: a.c
+ > Author: wangye
+ > Created Time: 2016年05月20日 星期五 00时48分46秒
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+
+
+int main()
+{
+	printf("hello world!");
+	return 0;
+}
